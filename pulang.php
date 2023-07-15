dc43b770ade50d282de64148257b17f0131a701c
haikal036 <139607327+haikal036@users.noreply.github.com>
2023-07-15 14:00:36 +0800

    Update pulang.php

--- pulang.php
+++ pulang.php
@@ -1,29 +1,26 @@
+<!DOCTYPE html>
 <html>
-<link rel="icon" href="images/logosekolah.png" type="images/png">
 <head>
-	 <link rel="stylesheet" href="assets/css/main.css" />
-     <link rel="stylesheet" href="assets/css/button.css" />
-     <link rel="stylesheet" href="assets/css/download.css" />
-     <link rel="stylesheet" href="assets/css/div2.css" />
-     <body style="background-color:#142952;">
-<title> SPBT 1.0.31 (beta) </title>
+    <link rel="icon" href="images/logosekolah.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/button.css">
+    <link rel="stylesheet" href="assets/css/download.css">
+    <link rel="stylesheet" href="assets/css/div2.css">
+    <style>
+        body {
+            background-color: #142952;
+        }
+    </style>
+    <title>SPBT 1.0.31 (beta)</title>
+</head>
 <body>
-
-<center><div><h2>SKIM PINJAMAN BUKU TEKS</h2>
-
-<h2>Panduan Pemulangan Buku</h2>
-
-<p>Sila kilik untuk memuat turun panduan.</p>
-
-<a href="panduan/murid.doc"  button class=button button ="window.print()"><h3>Muat turun Panduan</h3></button></a></center></div>
-
-
+    <center>
+        <div>
+            <h2>SKIM PINJAMAN BUKU TEKS</h2>
+            <h2>Panduan Pemulangan Buku</h2>
+            <p>Sila klik untuk memuat turun panduan.</p>
+            <a href="panduan/murid.doc" class="button" onclick="window.print()"><h3>Muat turun Panduan</h3></a>
+        </div>
+    </center>
 </body>
-</html>
-
-<center>
-	
-</body>
-</center>
-</table>
 </html>

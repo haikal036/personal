c266c818b6facc01e1aa6977f883638422fec3b8
haikal036 <139607327+haikal036@users.noreply.github.com>
2023-07-15 13:58:16 +0800

    Update Pelajar.php

--- Pelajar.php
+++ Pelajar.php
@@ -1,45 +1,48 @@
-
 <!DOCTYPE html>
 <html>
-<link rel="icon" href="images/logosekolah.png" type="images/png">
 <head>
-	 <link rel="stylesheet" href="assets/css/main.css" />
-     <link rel="stylesheet" href="assets/css/button.css" />
-     <body style="background-color:#142952;"></body>
-     <link rel="stylesheet" href="assets/css/div3.css" />
-     <link rel="stylesheet" href="assets/css/nav.css" />
-
-<title> SPBT 1.0.31 (beta) </title>
+    <link rel="icon" href="images/logosekolah.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/button.css">
+    <link rel="stylesheet" href="assets/css/div3.css">
+    <link rel="stylesheet" href="assets/css/nav.css">
+    <style>
+        body {
+            background-color: #142952;
+        }
+    </style>
+    <title>SPBT 1.0.31 (beta)</title>
+</head>
 <body>
+    <ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="patch.php">Maklumat Kemaskini</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li style="float:right"><a href="admin.php" class="active">ADMIN</a></li>
+    </ul>
 
-	<ul>
-  <li><a class="active" href="index.php">Home</a></li>
-  <li><a href="patch.php">Maklumat Kemaskini</a></li>
-  <li><a href="contact.php">Contact</a></li>
-  <li style="float:right"><a href="admin.php" class=active>ADMIN</a></li>
-</ul>
-
-<center><div>
-<h2><center>SKIM PINJAMAN BUKU TEKS</center></h2>
-<center><img src="images/logosekolah.png" alt="LOGO SEKOLAH" width="150" height="150">
-<img src="images/logoSPBT.jpg" alt="LOGO SPBT" width="150" height="150"></center>
-<center>
-
-<td>
-        <tr><h3>SENARAI TINGKATAN</h3><br><br>
-</td>
-
-<tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan1/Tingkatan1.php';" value="TINGKATAN 1" /></tr>
-<tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan2/tingkatan2.php';" value="TINGKATAN 2" /></tr>
-<tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan3/tingkatan3.php';" value=" TINGKATAN 3" /></tr>
-<tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan4/tingkatan4.php';" value="TINGKATAN 4" /></tr>
-<tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan5/tingkatan5.php';" value="TINGKATAN 5" /></tr>
-<tr><br><br><input type="button" class="button" onclick="window.location.href='Index.php';" value="MENU UTAMA" /></tr>
-
-
-
-</center>
-</div>
-</center>
-</table>
+    <center>
+        <div>
+            <h2>SKIM PINJAMAN BUKU TEKS</h2>
+            <center>
+                <img src="images/logosekolah.png" alt="LOGO SEKOLAH" width="150" height="150">
+                <img src="images/logoSPBT.jpg" alt="LOGO SPBT" width="150" height="150">
+            </center>
+            <br>
+            <td>
+                <tr>
+                    <h3>SENARAI TINGKATAN</h3>
+                    <br>
+                    <br>
+                </tr>
+            </td>
+            <tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan1/Tingkatan1.php';" value="TINGKATAN 1" /></tr>
+            <tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan2/tingkatan2.php';" value="TINGKATAN 2" /></tr>
+            <tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan3/tingkatan3.php';" value="TINGKATAN 3" /></tr>
+            <tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan4/tingkatan4.php';" value="TINGKATAN 4" /></tr>
+            <tr><input type="button" class="button" onclick="window.location.href='Student/Tingkatan5/tingkatan5.php';" value="TINGKATAN 5" /></tr>
+            <tr><br><br><input type="button" class="button" onclick="window.location.href='Index.php';" value="MENU UTAMA" /></tr>
+        </div>
+    </center>
+</body>
 </html>

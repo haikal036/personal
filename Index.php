24cd7fd113002d41ed9440815db3da723a0e0d0b
haikal036 <139607327+haikal036@users.noreply.github.com>
2023-07-15 13:54:53 +0800

    Update Index.php
    Better Syntax was applied

--- Index.php
+++ Index.php
@@ -1,37 +1,40 @@
-
 <!DOCTYPE html>
 <html>
-<link rel="icon" href="images/logosekolah.png" type="images/png">
 <head>
-	 <link rel="stylesheet" href="assets/css/main.css" />
-     <link rel="stylesheet" href="assets/css/button.css" />
-     <body style="background-color:#142952;"></body>
-     <link rel="stylesheet" href="assets/css/div2.css" />
-     <link rel="stylesheet" href="assets/css/nav.css" />
-
-<title> SPBT 1.0.40 (beta) </title>
+    <link rel="icon" href="images/logosekolah.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/button.css">
+    <link rel="stylesheet" href="assets/css/div2.css">
+    <link rel="stylesheet" href="assets/css/nav.css">
+    <style>
+        body {
+            background-color: #142952;
+        }
+    </style>
+    <title>SPBT 1.0.40 (beta)</title>
+</head>
 <body>
-<ul>
-	<li><a href="patch.php">Maklumat Kemaskini</a></li>
-  <li><a href="contact.php">Contact</a></li>
-  <li style="float:right"><a href="admin.php" class=active>ADMIN</a></li>
-</ul>
+    <ul>
+        <li><a href="patch.php">Maklumat Kemaskini</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li style="float:right"><a href="admin.php" class="active">ADMIN</a></li>
+    </ul>
 
-<center><div>
-<h2><center>SKIM PINJAMAN BUKU TEKS</center></h2>
-<center><img src="images/logosekolah.png" alt="LOGO SEKOLAH" width="150" height="150">
-<img src="images/logoSPBT.jpg" alt="LOGO SPBT" width="150" height="150"></center>
-<center>
-
-<td>
-        <tr><h3>MENU UTAMA</h3><br><br></tr>
-</td>
-
-<tr><input type="button" class="button" onclick="window.location.href='Pelajar.php';" value="PEMINJAMAN BUKU TEKS" /></tr>
-<tr><input type="button" class="button" onclick="window.location.href='pulang.php';" value="PEMULANGAN BUKU TEKS" /></tr>
-<br><br><tr><input type="button" class="button" onclick="window.location.href='Newpage.php';" value="KEMASKINI MAKLUMAT PELAJAR" /></tr>
-</center></div>
-
-</center>
-</table>
+    <center>
+        <div>
+            <h2>SKIM PINJAMAN BUKU TEKS</h2>
+            <center>
+                <img src="images/logosekolah.png" alt="LOGO SEKOLAH" width="150" height="150">
+                <img src="images/logoSPBT.jpg" alt="LOGO SPBT" width="150" height="150">
+            </center>
+            <br>
+            <h3>MENU UTAMA</h3>
+            <br>
+            <input type="button" class="button" onclick="window.location.href='Pelajar.php';" value="PEMINJAMAN BUKU TEKS" /><br>
+            <input type="button" class="button" onclick="window.location.href='pulang.php';" value="PEMULANGAN BUKU TEKS" /><br>
+            <br>
+            <input type="button" class="button" onclick="window.location.href='Newpage.php';" value="KEMASKINI MAKLUMAT PELAJAR" />
+        </div>
+    </center>
+</body>
 </html>

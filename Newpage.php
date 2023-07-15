60d76f2523cee3c6e92c8ef713bc2941aa381579
haikal036 <139607327+haikal036@users.noreply.github.com>
2023-07-15 13:56:47 +0800

    Update Newpage.php

--- Newpage.php
+++ Newpage.php
@@ -1,24 +1,28 @@
+<!DOCTYPE html>
 <html>
-<link rel="icon" href="images/logosekolah.png" type="images/png">
 <head>
-	 <link rel="stylesheet" href="assets/css/main.css" />
-     <link rel="stylesheet" href="assets/css/button.css" />
-     <link rel="stylesheet" href="assets/css/download.css" />
-     <link rel="stylesheet" href="assets/css/div3.css" />
-     <body style="background-color:#142952;"></body>
-<title> SPBT 1.0.31 (beta) </title>
+    <link rel="icon" href="images/logosekolah.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/button.css">
+    <link rel="stylesheet" href="assets/css/download.css">
+    <link rel="stylesheet" href="assets/css/div3.css">
+    <style>
+        body {
+            background-color: #142952;
+        }
+    </style>
+    <title>SPBT 1.0.31 (beta)</title>
+</head>
 <body>
-
-<center><div>
-<h2><center>SKIM PINJAMAN BUKU TEKS</center></h2>
-
-<br><center><img src="images/roboterror.png" alt="Construction" width="360" height="480"></center></br>
-<center>
-	
-
-</div>
-</center>
+    <center>
+        <div>
+            <h2>SKIM PINJAMAN BUKU TEKS</h2>
+            <br>
+            <center>
+                <img src="images/roboterror.png" alt="Construction" width="360" height="480">
+            </center>
+            <br>
+        </div>
+    </center>
 </body>
-</center>
-</table>
 </html>
